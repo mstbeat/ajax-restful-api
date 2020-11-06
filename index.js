d29d97ac6dbf3345b0ae4d00793d0c22d2908218
mstbeat <mrbrightside82@ymail.com>
2020-11-06 14:37:15 +0900

    finish basic app

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('testdb', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the database.');
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'));
+
+app.get('/users',function(req,res){
+  db.all("SELECT * FROM users",function(err,rows){
+    res.json(rows);
+  });
+})
+
+app.post('/users',function(req,res){
+  db.run("INSERT INTO users (name, pass) VALUES ('"+req.body.name+"','"+req.body.pass+"')",function(err){
+    console.log(err);
+    res.json({id:this.lastID});
+  });
+})
+
+app.get('/users/:id',function(req,res){
+  db.all("SELECT * FROM users WHERE id ="+req.params.id,function(err,rows){
+    res.json(rows);
+  });
+})
+
+app.put('/users/:id',function(req,res){
+  db.run("UPDATE users SET name ='"+req.body.name+"', pass='"+req.body.pass+"' WHERE id ="+req.params.id,function(err){
+    console.log(err);
+    res.json({status:this.changes});
+  });
+})
+
+app.delete('/users/:id',function(req,res){
+  db.run("DELETE FROM users WHERE id ="+req.params.id,function(err){
+    console.log(err);
+    res.json({status:this.changes});
+  });
+})
+
+app.listen(3000);
